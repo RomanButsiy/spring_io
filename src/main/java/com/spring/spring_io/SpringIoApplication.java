468905bd38b0f6b9dd6d0dbbde9caa45a318636f
RomanButsiy <butsiyroman@gmail.com>
2020-03-01 21:45:28 +0200

    Spring Init

---
+++ src/main/java/com/spring/spring_io/SpringIoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoApplication.class, args);
+	}
+
+}
