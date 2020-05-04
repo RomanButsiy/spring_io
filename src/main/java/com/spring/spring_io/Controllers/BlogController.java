28080ef90dfc863111691c7c487b1bbae47242fe
RomanButsiy <butsiyroman@gmail.com>
2020-05-04 17:45:55 +0300

    Added delete post

--- src/main/java/com/spring/spring_io/Controllers/BlogController.java
+++ src/main/java/com/spring/spring_io/Controllers/BlogController.java
@@ -23,7 +23,8 @@ public class BlogController {
 
     @GetMapping("/blog")
     public String blogMain(Model model) {
-        Iterable<Post> posts = postRepository.findAll();
+        ArrayList<Post> posts = (ArrayList<Post>) postRepository.findAllByOrderByIdDesc();
+        posts.removeIf(p -> p.isDeleted());
         model.addAttribute("title", "Блог");
         model.addAttribute("posts", posts);
         return "blog-main";
@@ -57,6 +58,43 @@ public class BlogController {
         model.addAttribute("title", res.get(0).getTitle());
         model.addAttribute("post", res);
         return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("title", "Редагування статті");
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String img, @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        post.setImg(img);
+        Date date= new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+        post.setMts(timestamp);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") long id) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setDeleted(true);
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        post.setMts(timestamp);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 
 }
--- src/main/java/com/spring/spring_io/Repo/PostRepository.java
+++ src/main/java/com/spring/spring_io/Repo/PostRepository.java
@@ -3,5 +3,8 @@ package com.spring.spring_io.Repo;
 import com.spring.spring_io.Models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findAllByOrderByIdDesc();
 }
